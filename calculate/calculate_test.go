587065cc4790d918ba4cca418198ad31e1f7e1c0
gnosthi <kenneth@yaay.us>
2019-04-11 23:08:41 -0500

    fixed testinng

--- calculate/calculate_test.go
+++ calculate/calculate_test.go
@@ -1,17 +1,17 @@
 package calculate
 
 import (
-	"testing"
-	"strconv"
 	"os"
+	"strconv"
+	"testing"
 )
 
 var calculatetest = []struct {
-	number     string
-	prime      bool
-	divisor    int
-	divisor2   int
-	err        error
+	number   string
+	prime    bool
+	divisor  uint64
+	divisor2 uint64
+	err      error
 }{
 	{"-1", false, 0, 0, nil},
 	{"1", true, 0, 0, nil},
@@ -19,41 +19,41 @@ var calculatetest = []struct {
 	{"3", true, 0, 0, nil},
 	{"4", false, 2, 2, nil},
 	{"5", true, 0, 0, nil},
-	{"6", false,3, 2, nil},
+	{"6", false, 3, 2, nil},
 	{"7", true, 0, 0, nil},
 	{"8", false, 4, 2, nil},
-	{"9", false,3,3,nil},
-	{"10", false,5,2,nil},
-	{"11", true,0,0,nil},
-	{"12",false,6,2,nil},
-	{"13",true,0,0,nil},
-	{"14",false,7,2,nil},
-	{"15",false,5,3,nil},
-	{"16",false,8,2,nil},
-	{"17",true,0,0,nil},
-	{"18",false,9,2,nil},
-	{"19",true,0,0,nil},
+	{"9", false, 3, 3, nil},
+	{"10", false, 5, 2, nil},
+	{"11", true, 0, 0, nil},
+	{"12", false, 6, 2, nil},
+	{"13", true, 0, 0, nil},
+	{"14", false, 7, 2, nil},
+	{"15", false, 5, 3, nil},
+	{"16", false, 8, 2, nil},
+	{"17", true, 0, 0, nil},
+	{"18", false, 9, 2, nil},
+	{"19", true, 0, 0, nil},
 	{"31", true, 0, 0, nil},
 	{"24", false, 12, 2, nil},
 	{"111", false, 37, 3, nil},
 }
 
 var divisortest = []struct {
-	in string
+	in  string
 	out int
 }{
 	{"1", 0},
 	{"10", 5},
 	{"20", 10},
-	{"7",0},
-	{"21",7},
-	{"33",11},
-	{"93",31},
-	{"111",37},
+	{"7", 0},
+	{"21", 7},
+	{"33", 11},
+	{"93", 31},
+	{"111", 37},
 }
 
-var nantest = []struct{
-	in string
+var nantest = []struct {
+	in  string
 	out bool
 }{
 	{"1", true},
@@ -67,11 +67,11 @@ var nantest = []struct{
 
 func TestCalculate(t *testing.T) {
 	for _, tt := range calculatetest {
-		t.Run(tt.number, func(t *testing.T){
-			n,_ := strconv.Atoi(tt.number)
-			isPrime, divisor, divisor2, err := Calculate(n)
+		t.Run(tt.number, func(t *testing.T) {
+			n, _ := strconv.Atoi(tt.number)
+			isPrime, divisor, divisor2, err := Calculate(uint64(n))
 			if isPrime != tt.prime {
-				t.Errorf("Error: Expected isPrime value of %v got %v",tt.prime, isPrime)
+				t.Errorf("Error: Expected isPrime value of %v got %v", tt.prime, isPrime)
 			}
 			if divisor != tt.divisor {
 				t.Errorf("Error: Expected divisor value of %v got %v", tt.divisor, divisor)
@@ -89,8 +89,8 @@ func TestCalculate(t *testing.T) {
 func BenchmarkCalculate(b *testing.B) {
 	for _, tt := range calculatetest {
 		b.Run(tt.number, func(b *testing.B) {
-			n,_ := strconv.Atoi(tt.number)
-			isPrime, divisor, divisor2, err := Calculate(n)
+			n, _ := strconv.Atoi(tt.number)
+			isPrime, divisor, divisor2, err := Calculate(uint64(n))
 			if isPrime != tt.prime {
 				b.Errorf("Error: Expected isPrime value of %v got %v", tt.prime, isPrime)
 			}
@@ -110,17 +110,17 @@ func BenchmarkCalculate(b *testing.B) {
 
 func TestCheckIfNumber(t *testing.T) {
 	for _, tt := range nantest {
-		t.Run(tt.in, func(t *testing.T){
+		t.Run(tt.in, func(t *testing.T) {
 			isNum := checkIfNumber(tt.in)
 			if isNum != tt.out {
 				t.Errorf("Error: Expected checkIfNumber to report %v, got %v instead", tt.out, isNum)
 			}
 		})
 	}
-	for i := 0; i < 1000; i ++ {
+	for i := 0; i < 1000; i++ {
 		isNum := checkIfNumber("a")
 		if isNum == true {
-			t.Errorf("Error: Expected false, got %t", isNum )
+			t.Errorf("Error: Expected false, got %t", isNum)
 		}
 	}
 	for i := 0; i < 1000; i++ {
@@ -133,9 +133,9 @@ func TestCheckIfNumber(t *testing.T) {
 
 func BenchmarkCheckIfNumber(b *testing.B) {
 	for _, tt := range nantest {
-		b.Run(tt.in, func(b *testing.B){
+		b.Run(tt.in, func(b *testing.B) {
 			isNum := checkIfNumber(tt.in)
-			if isNum != tt.out	{
+			if isNum != tt.out {
 				b.Errorf("Error: Expected checkIfNumber to report %v, got %v instead", tt.out, isNum)
 			}
 		})
@@ -156,13 +156,13 @@ func BenchmarkCheckIfNumber(b *testing.B) {
 
 func TestIsPrimePrint(t *testing.T) {
 	for _, tt := range calculatetest {
-		t.Run(tt.number, func(t *testing.T){
+		t.Run(tt.number, func(t *testing.T) {
 			isPrimePrint(tt.number)
 			isNotPrime(tt.number, tt.divisor, tt.divisor2)
 		})
 	}
 	for i := 0; i < 1000; i++ {
-		isPrime, divisor, divisor2, err := Calculate(i)
+		isPrime, divisor, divisor2, err := Calculate(uint64(i))
 		if err != nil {
 			t.Errorf("Error Occurred")
 		}
@@ -176,14 +176,14 @@ func TestIsPrimePrint(t *testing.T) {
 }
 
 func BenchMarkIsPrimePrint(b *testing.B) {
-	for _,tt := range calculatetest {
-		b.Run(tt.number, func(b *testing.B){
+	for _, tt := range calculatetest {
+		b.Run(tt.number, func(b *testing.B) {
 			isPrimePrint(tt.number)
 			isNotPrime(tt.number, tt.divisor, tt.divisor2)
 		})
 	}
 	for i := 0; i < b.N; i++ {
-		isPrime, divisor, divisor2, err := Calculate(i)
+		isPrime, divisor, divisor2, err := Calculate(uint64(i))
 		if err != nil {
 			b.Errorf("Error Occured")
 		}
@@ -191,26 +191,26 @@ func BenchMarkIsPrimePrint(b *testing.B) {
 			isPrimePrint(strconv.Itoa(i))
 		}
 		if isPrime != true {
-			isNotPrime(strconv.Itoa(i),divisor,divisor2)
+			isNotPrime(strconv.Itoa(i), divisor, divisor2)
 		}
 	}
 }
 
 func TestOperate(t *testing.T) {
-	os.Args = []string{"1", "111", "93", "abc", "31", "101","891","777","91","118","20394","19283","1928737","123","31","666"}
+	os.Args = []string{"1", "111", "93", "abc", "31", "101", "891", "777", "91", "118", "20394", "19283", "1928737", "123", "31", "666"}
 	Operate()
 	for i := 0; i < 1000; i++ {
 		os.Args = []string{strconv.Itoa(i)}
 		Operate()
 	}
-	for i := 0; i < 1000; i ++ {
+	for i := 0; i < 1000; i++ {
 		os.Args = []string{"a"}
 		Operate()
 	}
 }
 
 func BenchmarkOperate(b *testing.B) {
-	os.Args = []string{"1", "111", "93", "abc", "31", "101","891","777","91","118","20394","19283","1928737","123","31","666"}
+	os.Args = []string{"1", "111", "93", "abc", "31", "101", "891", "777", "91", "118", "20394", "19283", "1928737", "123", "31", "666"}
 	Operate()
 	for i := 0; i < b.N; i++ {
 		os.Args = []string{strconv.Itoa(i)}
